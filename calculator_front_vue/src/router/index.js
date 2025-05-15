a65e73c61cc2a6bdad7eb704cee8064e3c346415
Xavier <xavier970808@gmail.com>
2025-05-15 18:52:21 +0800

    first commit

--- calculator_front_vue/src/router/index.js
+++ calculator_front_vue/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import HomeView from '@/views/HomeView.vue'
 import 一般計算機 from '@/views/一般計算機.vue'
 import 函數繪圖 from '@/views/函數繪圖.vue'
 
--- calculator_front_vue/src/views/一般計算機.vue
+++ calculator_front_vue/src/views/一般計算機.vue
@@ -1,22 +1,49 @@
-<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-<script src="/calculator_front_vue/src/views/script.js"></script>
-
 <template>
-    <h1>工程計算機</h1>
-
-    <!-- 一般計算機 -->
-    <section>
-        <h2>一般計算</h2>
-        <input id="calc_input" type="text" size="50" placeholder="輸入算式，如 sin(30) + 5 * 2" style="height: 30px;">
-        <label>
-            模式:
-            <select id="calc_angle_mode" style="height: 30px;">
-                <option value="deg">角度</option>
-                <option value="rad">弧度</option>
-            </select>
-        </label>
-        <button onclick="calculate()" style="height: 30px;">計算</button>
-        <div id="calc_result"></div>
-    </section>
+  <h1>工程計算機</h1>
+  <section>
+    <h2>一般計算</h2>
+    <input v-model="expression" type="text" size="50" placeholder="輸入算式，如 sin(30) + 5 * 2" style="height: 30px;">
+    <label>
+      模式:
+      <select v-model="angleMode" style="height: 30px;">
+        <option value="deg">角度</option>
+        <option value="rad">弧度</option>
+      </select>
+    </label>
+    <button @click="calculate" style="height: 30px;">計算</button>
+    <div>{{ result }}</div>
+  </section>
 </template>
- 
+
+<script>
+const API_BASE = 'https://calculator-gj6q.onrender.com';
+
+export default {
+  data() {
+    return {
+      expression: '',
+      angleMode: 'deg',
+      result: ''
+    };
+  },
+  methods: {
+    async calculate() {
+      this.result = '計算中...';
+      try {
+        const response = await fetch(`${API_BASE}/calculate`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            expression: this.expression,
+            angle_mode: this.angleMode
+          })
+        });
+        const data = await response.json();
+        this.result = data.error ? `錯誤: ${data.error}` : `結果: ${data.result}`;
+      } catch (err) {
+        this.result = `伺服器連線失敗: ${err}`;
+      }
+    }
+  }
+};
+</script>
